e452ca7d46f01f8bb9730e408a797aa3c501bb4d
akrama23 <arundelfootball23@gmail.com>
2023-03-01 19:24:14 -0500

    finished nav bar links for shop and home

--- elite-clothing/src/App.js
+++ elite-clothing/src/App.js
@@ -1,17 +1,7 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
+import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
 
 const Shop = () => {
   return <hi>shop page</hi>;
@@ -21,7 +11,7 @@ const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route path="home" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
