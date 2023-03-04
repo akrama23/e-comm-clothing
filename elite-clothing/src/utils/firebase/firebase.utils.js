8f606bf78c9787d5912d9272f34bba4317b25cec
akrama23 <arundelfootball23@gmail.com>
2023-03-04 01:59:35 -0500

    adding firebase

--- elite-clothing/src/utils/firebase/firebase.utils.js
+++ elite-clothing/src/utils/firebase/firebase.utils.js
@@ -1,13 +1,26 @@
-import { initializeApp } from 'firebase/app';
-
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from " firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAHDdWAp9VlVytNhpwzpsXt6wq-IWLqrgI",
-    authDomain: "elite-clothing-db-2c3a0.firebaseapp.com",
-    projectId: "elite-clothing-db-2c3a0",
-    storageBucket: "elite-clothing-db-2c3a0.appspot.com",
-    messagingSenderId: "959056130076",
-    appId: "1:959056130076:web:a9ee5696c72265ac7bafd9"
-  };
-  
-  const firebaseApp = initializeApp(firebaseConfig);
+  apiKey: "AIzaSyAHDdWAp9VlVytNhpwzpsXt6wq-IWLqrgI",
+  authDomain: "elite-clothing-db-2c3a0.firebaseapp.com",
+  projectId: "elite-clothing-db-2c3a0",
+  storageBucket: "elite-clothing-db-2c3a0.appspot.com",
+  messagingSenderId: "959056130076",
+  appId: "1:959056130076:web:a9ee5696c72265ac7bafd9",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+
+export const auth = getAuth();
+export const signInwithGooglepopup = () => signInWithPopup(auth, provider);
