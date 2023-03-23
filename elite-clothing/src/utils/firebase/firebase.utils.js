12fc59e3ce7fd70212270a65eaa19ac7f7323cbf
akrama23 <arundelfootball23@gmail.com>
2023-03-23 00:07:03 -0400

    set up userdocref

--- elite-clothing/src/utils/firebase/firebase.utils.js
+++ elite-clothing/src/utils/firebase/firebase.utils.js
@@ -36,5 +36,10 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
 
+    console.log(userDocRef);
+
+    const userSnapShot = await getDoc(userDocRef)
+    console.log(userSnapShot)
 }
