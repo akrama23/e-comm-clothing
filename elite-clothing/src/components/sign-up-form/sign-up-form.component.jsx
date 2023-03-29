c4a9161a01a7b0d8a1a676f3beb779124d852892
akrama23 <arundelfootball23@gmail.com>
2023-03-29 00:45:53 -0400

    finished inital set up of sign up form

--- elite-clothing/src/components/sign-up-form/sign-up-form.component.jsx
+++ elite-clothing/src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,35 +1,63 @@
 import { useState } from "react";
 
 const defaultFormFields = {
-    displayName: '',
-    email: '',
-    password: '',
-    confirmPassword: ''
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
 };
 
 const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
 
-    const [formFields, setFormFields] = useState(defaultFormFields);
-    const { displayName, email, password, confirmPassword } = formFields;
+  console.log(formFields)
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-    return(
-        <div>
-            <h1>sign up with email and password</h1>
-            <form onSubmit={() => {}}>
-                <label>Display Name</label>
-                <input type="text" required/>
-                <label>Email</label>
-                <input type="email" required/>
-                <label>Password</label>
-                <input type="password" required/>
-                <label>Confirm Password</label>
-                <input type="password" required/>
-                <button type="submit"> Sign Up</button>
-            </form>
+    setFormFields({...formFields, [name]: value })
+  };
 
-        </div>
-    )
-
+  return (
+    <div>
+      <h1>sign up with email and password</h1>
+      <form onSubmit={() => {}}>
+        <label>Display Name</label>
+        <input
+          type="text"
+          required
+          onChange={handleChange}
+          name="displayName"
+          value={displayName}
+        />
+        <label>Email</label>
+        <input
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+        <label>Confirm Password</label>
+        <input
+          type="password"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
+        <button type="submit"> Sign Up</button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUpForm;
