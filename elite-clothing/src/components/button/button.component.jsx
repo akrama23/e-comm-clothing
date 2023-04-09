83881ad860bd8fce74d3c96184ad63254d12889a
akrama23 <arundelfootball23@gmail.com>
2023-04-08 23:38:55 -0400

    edits to syntax

--- elite-clothing/src/components/button/button.component.jsx
+++ elite-clothing/src/components/button/button.component.jsx
@@ -1,9 +1,5 @@
- const Button = ({childern}) => {
-    return(
-        <button className="button-container">
-            {childern}
-        </button>
-    )
- }
+const Button = ({ childern }) => {
+  return <button className="button-container">{childern}</button>;
+};
 
- export default Button;
+export default Button;
