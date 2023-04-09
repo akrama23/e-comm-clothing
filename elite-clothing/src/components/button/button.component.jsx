d9fb54a01515797c7130d29d59c7406a305966bb
akrama23 <arundelfootball23@gmail.com>
2023-04-08 23:51:20 -0400

    imported button and added styles to button' '

--- elite-clothing/src/components/button/button.component.jsx
+++ elite-clothing/src/components/button/button.component.jsx
@@ -1,15 +1,19 @@
-
+import "./button.styles.scss";
 
 const BUTTON_TYPES_CLASSES = {
-    google: 'google-sign-in',
-    inverted: 'inverted',
-
-
-}
-
+  google: "google-sign-in",
+  inverted: "inverted",
+};
 
 const Button = ({ childern, buttonType, ...otherProps }) => {
-  return <button className={`button-container ${BUTTON_TYPES_CLASSES[buttonType]}`} {...otherProps}>{childern}</button>;
+  return (
+    <button
+      className={`button-container ${BUTTON_TYPES_CLASSES[buttonType]}`}
+      {...otherProps}
+    >
+      {childern}
+    </button>
+  );
 };
 
 export default Button;
