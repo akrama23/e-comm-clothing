e9df123ffe8da4360c1c072fce2cce85aa19733b
akrama23 <arundelfootball23@gmail.com>
2023-04-08 23:46:39 -0400

    adding button type classes

--- elite-clothing/src/components/button/button.component.jsx
+++ elite-clothing/src/components/button/button.component.jsx
@@ -1,5 +1,15 @@
-const Button = ({ childern }) => {
-  return <button className="button-container">{childern}</button>;
+
+
+const BUTTON_TYPES_CLASSES = {
+    google: 'google-sign-in',
+    inverted: 'inverted',
+
+
+}
+
+
+const Button = ({ childern, buttonType, ...otherProps }) => {
+  return <button className={`button-container ${BUTTON_TYPES_CLASSES[buttonType]}`} {...otherProps}>{childern}</button>;
 };
 
 export default Button;
