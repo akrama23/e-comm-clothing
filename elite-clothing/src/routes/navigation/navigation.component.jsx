03ddc3fb17d258a292ece437b0096e640f567451
akrama23 <arundelfootball23@gmail.com>
2023-11-08 23:47:57 -0500

    refactoring snav

--- elite-clothing/src/routes/navigation/navigation.component.jsx
+++ elite-clothing/src/routes/navigation/navigation.component.jsx
@@ -9,12 +9,9 @@ import { signOutUser } from "../../utils/firebase/firebase.utils";
 import "./navigation.styles.scss";
 
 const Navigation = () => {
-  const { currentUser,setCurrentUser } = useContext(UserContext);
+  
+  const { currentUser } = useContext(UserContext);
 
-  const signOutHandler = async () => {
-    await signOutUser();
-    setCurrentUser(null)
-  }
 
   return (
     <Fragment>
@@ -28,7 +25,7 @@ const Navigation = () => {
             SHOP
           </Link>
           {currentUser ? (
-            <span className="nav-link" onClick={signOutHandler}>SIGN OUT</span>
+            <span className="nav-link" onClick={signOutUser}>SIGN OUT</span>
           ) : (
             <Link className="nav-link" to="/auth">
               SIGN IN
